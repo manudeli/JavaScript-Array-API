91a78a63800fee75eb281bf9f54d5d1c9b72e9c6
flowithus <flowithusdev@gmail.com>
2021-06-13 08:51:15 +0900

    Addition, Deletion, Copy

--- src/ArrayAPIs.js
+++ src/ArrayAPIs.js
@@ -26,3 +26,33 @@ for (let fruit of fruits) {
 }
 // c. forEach
 fruits.forEach((fruit) => console.log("forEach", fruit));
+
+// 4. Addition, Deletion, Copy
+// push: add an item to the end
+fruits.push("🍓", "🍐");
+console.log(fruits);
+
+// pop: remove an item from the end
+let lastFruit = fruits.pop();
+console.log(fruits, lastFruit);
+
+// unshift: add an item to the beginning
+fruits.unshift("🍓", "🍈");
+console.log(fruits);
+
+// shift: remove an item from the beginning
+let beginningFruit = fruits.shift();
+let beginningFruit2 = fruits.shift();
+console.log(beginningFruit2, beginningFruit, fruits);
+
+// NOTE!! shift, unshift are slower than pop, push
+// splice: remove an item by index position
+fruits.push("🍈", "🍓");
+console.log(fruits);
+fruits.splice(1, 1, "🍏", "🍉");
+console.log(fruits);
+
+// combine two arrays
+const fruits2 = ["🍐", "🥥"];
+const newFruits = fruits.concat(fruits2);
+console.log(newFruits);
