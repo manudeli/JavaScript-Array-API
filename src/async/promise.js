0230981419bc2744ceead9cd542841444ad93581
flowithus <flowithusdev@gmail.com>
2021-06-13 21:23:42 +0900

    Promise :: Chaining

--- src/async/promise.js
+++ src/async/promise.js
@@ -26,3 +26,18 @@ promise //
   .finally(() => {
     console.log("finally");
   });
+
+// TODO: 3. Promise chaining
+const fetchNumber = new Promise((resolve, reject) => {
+  setTimeout(() => resolve(1), 1000);
+});
+
+fetchNumber
+  .then((num) => num * 2)
+  .then((num) => num * 3)
+  .then((num) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(num - 1), 1000);
+    });
+  })
+  .then((num) => console.log(num));
