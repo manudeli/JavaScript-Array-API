266678f93f4a21a2f38f01904ba2250fb8490c81
flowithus <flowithusdev@gmail.com>
2021-06-13 19:59:41 +0900

    Callback Hell Example

--- src/async/callback.js
+++ src/async/callback.js
@@ -19,3 +19,51 @@ function printWithDelay(print, timeout) {
   setTimeout(print, timeout);
 }
 printWithDelay(() => console.log("async callback"), 2000);
+
+// Callback Hell example
+class UserStorage {
+  loginUser(id, password, onSuccess, onError) {
+    setTimeout(() => {
+      if (
+        (id === "jonghyeon" && password === "123456") ||
+        (id === "coder" && password === "123456")
+      ) {
+        onSuccess(id);
+      } else {
+        onError(new Error("not found"));
+      }
+    }, 2000);
+  }
+
+  getRoles(user, onSuccess, onError) {
+    setTimeout(() => {
+      if (user === "jonghyeon") {
+        onSuccess({ name: "jonghyeon", role: "admin" });
+      } else {
+        onError(new Error("no access"));
+      }
+    }, 1000);
+  }
+}
+
+const userStorage = new UserStorage();
+const id = prompt("enter your id");
+const password = prompt("enter your password");
+userStorage.loginUser(
+  id,
+  password,
+  (user) => {
+    userStorage.getRoles(
+      user,
+      (userWithRole) => {
+        alert(
+          `Hello ${userWithRole.name}, you have a ${userWithRole.role} role`
+        );
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  },
+  console.log
+);
